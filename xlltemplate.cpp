ffb7498f66009e5094d044463f5265ae8bc07137
Keith A. Lewis <keithalewis@live.com>
2018-12-20 22:10:37 -0500

    FInal

--- xlltemplate.cpp
+++ xlltemplate.cpp
@@ -21,22 +21,120 @@ inline X mean(const std::function<X()>& f, size_t n)
 	return m;
 }
 
+
 void test_jump_diffusion_cdf()
 {
 	//??? Question 6. Use std::normal_distribution and std::poisson_distribution to test jump_diffusion_cdf.
+	std::default_random_engine dre;
+	double a = 0.9;
+	double lambda = 0.1;
+	fms::jump_diffusion X(a, lambda);
+	double mean = 0;
+	double z = 0;
+	size_t N = 1000;
+	for (size_t n = 1; n <= N; ++n)
+	{
+		mean += (1 * (X.variate(dre) <= z) - mean) / n;
+	}
+	double cdf = X.cdf(z);
+	ensure(fabs(mean - cdf) < 1 / sqrt(N));
 }
 test xai_test_jump_diffusion_cdf(test_jump_diffusion_cdf);
 
+// SKIP
 void test_jump_diffusion_implied()
 {
 	//??? Question 7. Test jump_diffusion::implied for the following implied volatilities
 	double s[] = { 0.5, 0.1, 0.2, 0.5 };
 }
-test xai_test_jump_diffusion_implied(test_jump_diffusion_implied);
+//test xai_test_jump_diffusion_implied(test_jump_diffusion_implied);
 
 //??? Questing 8. Write an add-in called FMS.JUMP_DIFFUSION that returns a handle to a fms::jump_diffusion object
+static AddIn xai_jump_diffusion(
+	Function(XLL_HANDLE, L"?xll_jump_diffusion", L"FMS.JUMP_DIFFUSION")
+	.Arg(XLL_DOUBLE, L"a", L"is the dffusion parameter.")
+	.Arg(XLL_DOUBLE, L"lambda", L"is the jump parameter.")
+	.Uncalced()
+	.FunctionHelp(L"Return a handle to a jump diffustion model.")
+	.Category(L"XLL")
+	.Documentation(L"Docs")
+);
+HANDLEX WINAPI xll_jump_diffusion(double a, double lambda)
+{
+#pragma XLLEXPORT
+	handlex result;
 
+	try {
+		handle<fms::jump_diffusion> h(new fms::jump_diffusion(a, lambda));
+		result = h.get();
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return result;
+}
 //??? Questing 9. Write an add-in called FMS.JUMP_DIFFUSION.VALUE that uses the handle above to value a binary put option.
+static AddIn xai_jump_diffusion_value(
+	Function(XLL_HANDLE, L"?xll_jump_diffusion_value", L"FMS.JUMP_DIFFUSION.VALUE")
+	.Arg(XLL_HANDLE, L"handle", L"is a handle returned by FMS.JUMP_DIFFUSION>")
+	.Arg(XLL_DOUBLE, L"f", L"is the forward of the underlying.")
+	.Arg(XLL_DOUBLE, L"s", L"is the volatility parameter.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike of the binary option.")
+	.FunctionHelp(L"Returns the binary put forward value of a jump diffustion model.")
+	.Category(L"XLL")
+	.Documentation(L"Docs")
+);
+double WINAPI xll_jump_diffusion_value(HANDLEX h, double f, double s, double k)
+{
+#pragma XLLEXPORT
+	double result = std::numeric_limits<double>::quiet_NaN();
+
+	try {
+		handle<fms::jump_diffusion> h_(h);
+		ensure(h_);
+
+		result = h_->value(f, s, k);
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return result;
+}
+
+std::default_random_engine dre;
+
+static AddIn xai_jump_diffusion_variate(
+	Function(XLL_HANDLE, L"?xll_jump_diffusion_variate", L"FMS.JUMP_DIFFUSION.VARIATE")
+	.Arg(XLL_HANDLE, L"handle", L"is a handle returned by FMS.JUMP_DIFFUSION>")
+	.Arg(XLL_DOUBLE, L"f", L"is the forward of the underlying.")
+	.Arg(XLL_DOUBLE, L"s", L"is the volatility parameter.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike of the binary option.")
+	.Volatile()
+	.FunctionHelp(L"Returns a random value of the underlying.")
+	.Category(L"XLL")
+	.Documentation(L"Docs")
+);
+double WINAPI xll_jump_diffusion_variate(HANDLEX h, double f, double s, double k)
+{
+#pragma XLLEXPORT
+	double result = std::numeric_limits<double>::quiet_NaN();
+
+	try {
+		handle<fms::jump_diffusion> h_(h);
+		ensure(h_);
+
+		double X = h_->variate(dre);
+
+		result = f * exp(s*X - h_->kappa(s));
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+	}
+
+	return result;
+}
 
 //??? Questing 10. Create a spreadsheet that values a binary put option with parameters a = .9, lambda = .1, f = 100, s = 0.2, and k = 100.
 // Use xllmonte to compare the closed form and Monte Carlo values.
--- xlltemplate.h
+++ xlltemplate.h
@@ -1,4 +1,5 @@
 // xlltemplate.h
+#include <math.h>
 #include "xll12/xll/xll.h"
 #include "xll12/xll/ensure.h"
 
@@ -8,7 +9,10 @@ namespace fms {
 
 	inline double normal_cdf(double z)
 	{
-		return 0; //???Question 2. Return the standard normal cumulative distribution.
+		//return 0; //???Question 2. Return the standard normal cumulative distribution.
+		static double sqrt2 = sqrt(2);
+
+		return 0.5 + erf(z / sqrt2) / 2;
 	}
 
 	class jump_diffusion {
@@ -19,22 +23,62 @@ namespace fms {
 		{
 			// ensure a and lambda are valid
 
-			b = 0; //??? put Question 1 formula here*/)
+			//b = 0; //??? put Question 1 formula here*/)
+			ensure(0 < a && a <= 1);
+			ensure(lambda > 0);
+
+			b = (1 - a * a) / lambda; // or -(1 - a*a)/lambda
 		}
+
 		double cdf(double z)
 		{
-			return 0; //??? Question 3. Return P(X <= z) where X = a X + b (N - lambda)
+			//return 0; //??? Question 3. Return P(X <= z) where X = a X + b (N - lambda)
+
+			// P(X <= z) = P(aZ + b(N - lambda) <= z)
+			//           = P(Z <= (z - b(N - lambda))/a)
+			//           = sum_{n>=0} P(Z <= (z - b(n - lambda))/a) lambda^n/n! exp(-lambda)
+			double lambda_n = exp(-lambda); // lambda^n/n! exp(-lambda)
+			double sum = normal_cdf((z - b * (-lambda)) / a)*lambda_n; // first term
+			for (size_t n = 1; n < 100/* safeguard */; ++n) {
+				lambda_n *= lambda / n;
+				double term = normal_cdf((z - b * (n - lambda)) / a)*lambda_n;
+				if (term + 1 == 1)
+					break;
+				sum += term;
+			}
+
+			return sum;
+		}
+		// kappa^Z(s) = s^2/2
+		// kappa^N(s) = lambda(exp(s) - 1)
+		// kappa^{a Z + b (N - lambda)}(s) = a*a*s*s/2 + lambda(exp(bs) - 1) = lambda*b*s
+		double kappa(double s)
+		{
+			return a * a*s*s / 2 + lambda * (exp(b*s) - 1) - lambda*b*s;
 		}
 		// Binary put forward value
 		double value(double f, double s, double k)
 		{
-			return 0; //???Question 4. Return E 1(F <= k)
+			//return 0; //???Question 4. Return E 1(F <= k)
+			// z = (kappa(s) + log k/f)/s
+			double z = (kappa(s) + log(k/f)) / s;
+
+			return cdf(z);
 		}
+		// SKIP
 		// Binary put implied volatility using
 		// https://github.com/keithalewis/MATHGR5260_001_2018/blob/master/GR5260/fms_root1d_newton.h
 		double implied(double f, double v, double k)
 		{
 			return 0; //??Question 5. Return s such that v = value(s, k)
+		}
+		template<class R>
+		double variate(R& rng)
+		{
+			std::normal_distribution<> Z;
+			std::poisson_distribution<> N{ lambda };
+
+			return a * Z(rng) + b * (N(rng) - lambda);
 		}
 	};
 
